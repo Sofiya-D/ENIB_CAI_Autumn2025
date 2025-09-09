960a4c716dd7cf32bd6ad05b381fa7ccc2ede23e
Escargot <sofiya.savarovska@gmail.com>
2025-09-09 15:55:14 +0200

    workspace setup

---
+++ OfflineFolderSync/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="offlinefoldersync",
+    version="0.1.0",
+    packages=["offlinefoldersync"],
+    author="Sofiya Debois",
+    install_requires=[
+        "PyQt5",
+    ],
+)
